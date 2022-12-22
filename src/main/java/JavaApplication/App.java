9f1c49cc172f2d461a9e84a146fda0931be80a00
apo-axelor <a.popat@axelor.com>
2022-12-22 13:14:57 +0530

    my first commit

---
+++ src/main/java/JavaApplication/App.java
@@ -0,0 +1,9 @@
+package Java_application;  
+public class App {  
+    public String getGreeting() {  
+        return "Hello world.";  
+    }  
+    public static void main(String[] args) {  
+        System.out.println(new App().getGreeting());  
+    }  
+}  
